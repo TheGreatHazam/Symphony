51261614bf55d389f539b8fc1c6ed9e82f166b91
JosephHong3145 <dot7201@gmail.com>
2021-11-24 06:53:43 -0500

    can reach dashboard page

--- spotify_app/client/src/components/RedirectPage.js
+++ spotify_app/client/src/components/RedirectPage.js
@@ -1,5 +1,30 @@
 import React from 'react';
-const RedirectPage = () => {
-    return <div>Redirect Page</div>;
-};
-export default RedirectPage;
+import _ from 'lodash';
+import { getParamValues } from '../utils/functions';
+
+export default class RedirectPage extends React.Component {
+    componentDidMount() {
+        const { setExpiryTime, history, location } = this.props;
+        try {
+            if (_.isEmpty(window.parent.location.hash)) {
+                return window.location = "/dashboard";
+            }
+            const access_token = getParamValues(window.parent.location.hash);
+            const expiryTime = new Date().getTime() + access_token.expires_in * 1000;
+            localStorage.setItem('params', JSON.stringify(access_token));
+            localStorage.setItem('expiry_time', expiryTime);
+            window.location = "/dashboard"
+        } catch (error) {
+            window.location = "/"
+            console.log(error);
+        }
+    }
+
+    render() {
+        return(
+            <div>
+                Redirecting...
+            </div>
+        )
+    }
+}
