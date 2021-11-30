c3b06cb265d841e774d27037687e957dcbe1a03e
JosephHong3145 <dot7201@gmail.com>
2021-11-29 23:43:31 -0500

    display playlist list and music list

--- spotify_app/client/src/components/SpotifyGetPlaylist.js
+++ spotify_app/client/src/components/SpotifyGetPlaylist.js
@@ -4,6 +4,9 @@ import {Button} from "@material-ui/core";
 import {getParamValues} from "../utils/functions";
 
 const playlists_endpoint = "https://api.spotify.com/v1/me/playlists/";
+let playlistList = " ";
+let playlistEndpointArray = [];
+let listOfSongs = " ";
 
 const SpotifyGetPlaylist = () => {
     const [token, setToken] = useState('');
@@ -16,6 +19,7 @@ const SpotifyGetPlaylist = () => {
     }, [])
 
     const handleGetPlaylist = () => {
+
         console.log("Token: " + token);
         axios.get(playlists_endpoint, {
             headers: {
@@ -23,25 +27,55 @@ const SpotifyGetPlaylist = () => {
             }
         }).then(r => {
             setData(r.data);
-            console.log(r.data.items[0].tracks);
-            return(
-                <>
-                    <Button variant="info" type="submit" onClick={handleGetPlaylist}>Get Playlists</Button>
-                    {r.data.items ? r.data.items.map((items) => <p>(item.name)</p>) : null}
-                </>
-            )
+            console.log(r.data.items);
+            playlistList = " ";
+            playlistList += "User's playlists: ";
+            const playlistLength = r.data.items.length;
+            for (let i = 0; i < playlistLength; i++) {
+                playlistList += r.data.items[i].name + ", ";
+            }
+
+            for (let i = 0; i < playlistLength; i++) {
+                let playlistID = r.data.items[i].id;
+                playlistEndpointArray[i] = "https://api.spotify.com/v1/playlists/" + playlistID + "/tracks";
+            }
         })
             .catch((error) => {
                 console.log(error);
+        })
+
+        listOfSongs = " ";
+        listOfSongs += "List of songs: ";
+        for (let i = 0; i < playlistEndpointArray.length; i++) {
+            axios.get(playlistEndpointArray[i], {
+                headers: {
+                    Authorization: "Bearer " + token,
+                }
+            }).then(r => {
+                console.log(r.data);
+                let totalSong = r.data.total;
+
+                for (let i = 0; i < totalSong; i++) {
+                    listOfSongs += r.data.items[i].track.name + ", ";
+                }
+            }).catch((error) => {
+                console.log(error);
             })
+        }
     }
 
     return (
         <>
             <Button variant="info" type="submit" onClick={handleGetPlaylist}>Get Playlists</Button>
-            {data?.items ? data.items.map((item) => <p>(item.name)</p>) : null}
+            <div>
+                {playlistList}
+            </div>
+            <div>
+                {listOfSongs}
+            </div>
         </>
-    )
+    );
+
 }
 
 export default SpotifyGetPlaylist;
