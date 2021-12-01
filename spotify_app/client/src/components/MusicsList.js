0ae6162cad860fee2cba44177a0a44f4ba7f32da
JosephHong3145 <dot7201@gmail.com>
2021-12-01 11:40:12 -0500

    fully working playlist function

--- spotify_app/client/src/components/MusicsList.js
+++ spotify_app/client/src/components/MusicsList.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import {Dropdown, Table} from 'react-bootstrap'
 
-const addToPlaylist = () => {
+let listOfPlayList = [];
+let playlistID = 0;
+
+const addToPlaylist = (music) => {
     return(
         <Dropdown>
             <Dropdown.Toggle variant="success" id="dropdown-basic">
@@ -9,12 +12,38 @@ const addToPlaylist = () => {
             </Dropdown.Toggle>
 
             <Dropdown.Menu>
-                <Dropdown.Item>New Playlist</Dropdown.Item>
-                <Dropdown.Item>Playlist 1</Dropdown.Item>
-                <Dropdown.Item>Playlist 2</Dropdown.Item>
+                <Dropdown.Item onClick={() => createNewPlaylist(music)}>New Playlist</Dropdown.Item>
+                {listOfPlayList.length >= 1 ? (<Dropdown.Item onClick = {() => addToExistingPlaylist(music, 0)}>Playlist 1</Dropdown.Item>) : (<></>)}
+                {listOfPlayList.length >= 2 ? (<Dropdown.Item onClick = {() => addToExistingPlaylist(music, 1)}>Playlist 2</Dropdown.Item>) : (<></>)}
+                {listOfPlayList.length >= 3 ? (<Dropdown.Item onClick = {() => addToExistingPlaylist(music, 2)}>Playlist 3</Dropdown.Item>) : (<></>)}
+                {listOfPlayList.length >= 4 ? (<Dropdown.Item onClick = {() => addToExistingPlaylist(music, 3)}>Playlist 4</Dropdown.Item>) : (<></>)}
+                {listOfPlayList.length >= 5 ? (<Dropdown.Item onClick = {() => addToExistingPlaylist(music, 4)}>Playlist 5</Dropdown.Item>) : (<></>)}
+
             </Dropdown.Menu>
         </Dropdown>
     )
+}
+
+const createNewPlaylist = (music) => {
+    let playListName = " ";
+    let listOfMusic = [];
+    let playlist = {playListName, listOfMusic};
+
+    listOfMusic.push(music);
+    playListName = 'Playlist' + playlistID.toString();
+
+    playlist.playListName = playListName;
+    playlist.listOfMusic = listOfMusic;
+
+    listOfPlayList.push(playlist);
+
+    playlistID += 1;
+
+    console.log(listOfPlayList);
+}
+
+const addToExistingPlaylist = (music, i) => {
+    listOfPlayList[i].listOfMusic.push(music);
 }
 
 const Musics = ({ musics }) => {
@@ -52,7 +81,7 @@ const Musics = ({ musics }) => {
 
                                         <td>{music.artists.map((artist) => artist.name).join(', ')}</td>
 
-                                        <td>{addToPlaylist()}</td>
+                                        <td>{addToPlaylist(music)}</td>
                                      </React.Fragment>
                                     </tr>
                                 );
